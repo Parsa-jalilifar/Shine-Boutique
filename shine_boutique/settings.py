3b939f73cc55452e0487823032791304d675c53a
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-08-22 15:28:02 -0400

    bug3

--- shine_boutique/settings.py
+++ shine_boutique/settings.py
@@ -122,9 +122,10 @@ USE_TZ = True
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 MEDIA_URL = '/images/'
 
