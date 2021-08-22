7d119556598a668f69b7584ad230234d829aa90b
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-08-22 15:17:17 -0400

    template error fixed

--- shine_boutique/settings.py
+++ shine_boutique/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'shine_boutique.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
