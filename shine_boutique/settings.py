35309e8a09c972e49e5d3032a49ab3286630ecf4
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-08-22 15:05:54 -0400

    bug

--- shine_boutique/settings.py
+++ shine_boutique/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'shine_boutique.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(SETTINGS_PATH, 'templates/store')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
